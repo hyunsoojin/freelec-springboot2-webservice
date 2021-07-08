df422e1f829d28623e9ef228db69583222ce62cc
Jinny <jhs7251@naver.com>
2021-07-08 14:37:47 +0900

    register src

---
+++ src/test/java/com/jinnysmain/book/springboot/ApplicationTest.java
@@ -0,0 +1,19 @@
+package com.jinnysmain.book.springboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+public class ApplicationTest {
+
+    @Test
+    public void main() {
+
+    }
+}
