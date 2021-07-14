1525ab8f70d3b9c230c2e4b5364c8237fd51ec9d
Jinny <jhs7251@naver.com>
2021-07-14 17:10:27 +0900

    update springboot2 project

--- src/main/java/com/jinnysmain/book/springboot/web/IndexController.java
+++ src/main/java/com/jinnysmain/book/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.jinnysmain.book.springboot.web;
 
+import com.jinnysmain.book.springboot.config.auth.LoginUser;
+import com.jinnysmain.book.springboot.config.auth.dto.SessionUser;
 import com.jinnysmain.book.springboot.domain.posts.PostsRepository;
 import com.jinnysmain.book.springboot.services.posts.PostsService;
 import com.jinnysmain.book.springboot.web.dto.PostsResponseDto;
@@ -17,8 +19,11 @@ public class IndexController {
     private final PostsService postsService;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
